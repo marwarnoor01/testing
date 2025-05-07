3e1273c6546fc73d9e6b57d17ff31c50df5a1aa4
marwarnoor01 <marwanoor398@gmail.com>
2025-05-07 03:25:43 -0700

    initaial Commit
    Uploading our first to github

---
+++ src/Components/Textchanger.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Textchanger = () => {
+  return (
+    <div>
+      <div className="w-[85%]" m-auto mt-9>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Textchanger
